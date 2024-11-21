38b1ce40fdf413ef382acf3ddd974c69a10008ee
Yousra Diab <yousserah@gmail.com>
2024-11-21 15:30:26 +0100

    terraform VM

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.10.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
